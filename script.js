1c19d52191fd669654fccddefc4f01f953f200ee
MoriumAkter123 <morium2312091068@diu.edu.bd>
2024-10-17 08:51:40 +0600

    class-26

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("This is first class of javascript");
+
+        //var x=50;
+        let y=20;
+       //let x=40;
+       //var x= 50;
+       //let x;
+       //x=30;
+       x=40;
+       const z =30;
+       //z=40;
+       console.log(z+x);
+        var Result=x+y;
+        //console.log(Result);
+        var rahim = 2000;
+        var purchase =1000;
+        var remaining =rahim-purchase;
+        console.log(remaining);
